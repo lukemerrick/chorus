9ef0c5b20b4fd0691c91e8b1e074d34f4793aef5
Luke Merrick <contact@lukemerrick.com>
2017-04-28 00:35:27 -0400

    finished basic simulation

--- additions.py
+++ additions.py
@@ -6,7 +6,16 @@ def get_phase(data, new_volume = 1.0):
         needed to keep the sound going without popping sounds
     '''
     downstroke = data[-2] - data[-1] > 0
-    raw_phase = np.arcsin(data[-1] / new_volume) / (math.pi*2)
+    raw_phase = 0
+    # in case volume makes this matching impossible
+    if (data[-1] / new_volume) > 1:
+        # print('cannot match phase')
+        raw_phase = .25
+    elif (data[-1] / new_volume) < -1:
+        # print('cannot match phase')
+        raw_phase = -.25
+    else:
+        raw_phase = np.arcsin(data[-1] / new_volume) / (math.pi*2)
     if downstroke:
         if data[-1] > 0:
             return(.5-raw_phase)
